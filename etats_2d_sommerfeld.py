f3df6c0ce1e39f408a6c1122f63e2f9ae100996a
David Cassagne <github@davidcassagne.com>
2016-05-11 17:22:22 +0200

    Create etats_2d_sommerfeld.py

---
+++ etats_2d_sommerfeld.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 17 14:20:38 2016
+
+@author: cassagne
+"""
+
+from pylab import *
+
+L = 3
+
+
+xmin = 0
+xmax = L
+nbx = 41
+ymin = 0
+ymax = L
+nby = 41
+
+nx = 1
+ny = 3
+
+kx = nx * pi/L
+ky = ny * pi/L
+
+
+x = linspace(xmin, xmax, nbx)
+y = linspace(ymin, ymax, nby)
+X, Y = meshgrid(x, y)
+
+Z = sin(kx * X) *sin(ky * Y)
+
+imshow(Z, interpolation="bicubic", 
+       origin="lower", extent=[xmin,xmax,ymin,ymax])
+colorbar()
+
+show()
